995e737e0db359605ae041e0c69c47c0e33a3d16
David <davatata@gmail.com>
2017-03-03 16:42:45 -0800

    v0.1
    simple layout

---
+++ flux.js
@@ -0,0 +1,11 @@
+// when certian button (in or out) is clicked, toggle backgrounds
+function flux_click(id) {
+    var in_button = document.getElementById("bottom_in");
+    var out_button = document.getElementById("bottom_out");
+    in_button.style.backgroundColor = "white";
+    out_button.style.backgroundColor = "white";
+    in_button.style.color = "red";
+    out_button.style.color = "red";
+    $(id).css("background-color", "red");
+    $(id).css("color", "white");
+}
