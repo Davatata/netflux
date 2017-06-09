e8b5f11935116e9cb76062fb6c96e128c558a251
David <davatata@gmail.com>
2017-06-09 16:07:28 -0700

    changed spacing

--- flux.js
+++ flux.js
@@ -1,16 +1,17 @@
 var months = {
-    "1":"January",
-    "2": "February",
-    "3": "March",
-    "4": "April",
-    "5": "May",
-    "6": "June",
-    "7": "July",
-    "8": "August",
-    "9": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December" };
+  "1": "January",
+  "2": "February",
+  "3": "March",
+  "4": "April",
+  "5": "May",
+  "6": "June",
+  "7": "July",
+  "8": "August",
+  "9": "September",
+  "10": "October",
+  "11": "November",
+  "12": "December"
+};
 
 var chosen_month;
 var possible_month;
@@ -18,101 +19,101 @@ var current_flux = "in";
 
 // set current month
 function setMonths() {
-    chosen_month = new Date().getMonth() + 1;
-    possible_month = chosen_month;
-    $(".month-item").text(months[chosen_month]);
-    changeMonth(1);
+  chosen_month = new Date().getMonth() + 1;
+  possible_month = chosen_month;
+  $(".month-item").text(months[chosen_month]);
+  changeMonth(1);
 }
 
 function changeMonth(n) {
-    if (n === 1) {
-        $(".month-item").text(months[possible_month]);
-        chosen_month = possible_month;
-        var file_month = months[chosen_month].toLowerCase().substring(0,3);
-        console.log(file_month);
+  if (n === 1) {
+    $(".month-item").text(months[possible_month]);
+    chosen_month = possible_month;
+    var file_month = months[chosen_month].toLowerCase().substring(0, 3);
+    console.log(file_month);
 
-        if (current_flux === "in") {
-            var file_string = "https://raw.githubusercontent.com/Davatata/netflux/master/" + "incoming/in_"+file_month+"_2017.txt";
-        } else {
-            var file_string = "https://raw.githubusercontent.com/Davatata/netflux/master/" + "outgoing/out_"+file_month+"_2017.txt";
-        }
-
-        var ele = createNode(file_string);
-        console.log(ele);
-        var parent = document.getElementById("parent_node");
-        var content = document.getElementById("flux_content");
-        ele.id = "flux_content";
-        ele.className = "white-background";
-        parent.replaceChild(ele, content);
-
+    if (current_flux === "in") {
+      var file_string = "https://raw.githubusercontent.com/Davatata/netflux/master/" + "incoming/in_" + file_month + "_2017.txt";
     } else {
-        possible_month = chosen_month;
+      var file_string = "https://raw.githubusercontent.com/Davatata/netflux/master/" + "outgoing/out_" + file_month + "_2017.txt";
     }
 
-    $('button.close-button').trigger('click');
+    var ele = createNode(file_string);
+    console.log(ele);
+    var parent = document.getElementById("parent_node");
+    var content = document.getElementById("flux_content");
+    ele.id = "flux_content";
+    ele.className = "white-background";
+    parent.replaceChild(ele, content);
+
+  } else {
+    possible_month = chosen_month;
+  }
+
+  $('button.close-button').trigger('click');
 }
 
 function pickMonth(n) {
-    possible_month = n;
+  possible_month = n;
 }
 
 // when certian button (in or out) is clicked, toggle backgrounds
 function flux_click(id) {
-    var selected = id.id.split("_")[1];
-    var unselect = selected === "in" ? "out" : "in";
-    current_flux = selected;
+  var selected = id.id.split("_")[1];
+  var unselect = selected === "in" ? "out" : "in";
+  current_flux = selected;
 
-    $("#bottom_" + selected).removeClass("white_button").addClass("red_button");
-    $("#bottom_" + unselect).removeClass("red_button").addClass("white_button");
+  $("#bottom_" + selected).removeClass("white_button").addClass("red_button");
+  $("#bottom_" + unselect).removeClass("red_button").addClass("white_button");
 
-    $("#topbar_" + selected).removeClass("white-text").addClass("pink-text");
-    $("#topbar_" + unselect).removeClass("pink-text").addClass("white-text");
-    changeMonth(1);
-    document.getElementById(id.id).blur();
+  $("#topbar_" + selected).removeClass("white-text").addClass("pink-text");
+  $("#topbar_" + unselect).removeClass("pink-text").addClass("white-text");
+  changeMonth(1);
+  document.getElementById(id.id).blur();
 }
 
 function createNode(file) {
-    console.log(file);
-    var node = document.createElement("div");
+  console.log(file);
+  var node = document.createElement("div");
 
-    var jqxhr = $.get(file, function(data) {
+  var jqxhr = $.get(file, function (data) {
 
-    })
-    .done(function(data) {
-        var days = data.split("\n\n");
-        for (var i = 0; i < days.length; i++) {
-            var items = days[i].trim().split("\n");
-            var header = document.createElement("h3");
-            header.appendChild(document.createTextNode(items[0]));
-            header.setAttribute("class", "date-header");
+  })
+    .done(function (data) {
+      var days = data.split("\n\n");
+      for (var i = 0; i < days.length; i++) {
+        var items = days[i].trim().split("\n");
+        var header = document.createElement("h3");
+        header.appendChild(document.createTextNode(items[0]));
+        header.setAttribute("class", "date-header");
 
-            var list = document.createElement("ul");
-            // list.setAttribute("style", "list-style-type: none");
-            for (var j = 1; j < items.length; j++) {
-                var list_item = document.createElement("li");
-                list_item.appendChild(document.createTextNode(items[j]));
-                list.appendChild(list_item);
-            }
-            node.appendChild(header);
-            node.appendChild(list);
+        var list = document.createElement("ul");
+        // list.setAttribute("style", "list-style-type: none");
+        for (var j = 1; j < items.length; j++) {
+          var list_item = document.createElement("li");
+          list_item.appendChild(document.createTextNode(items[j]));
+          list.appendChild(list_item);
         }
-        console.log(node);
+        node.appendChild(header);
+        node.appendChild(list);
+      }
+      console.log(node);
     })
-    .fail(function(data) {
-        var bad = document.createElement("h4");
-        bad.appendChild(document.createTextNode("Coming Soon."));
-        node.appendChild(bad);
+    .fail(function (data) {
+      var bad = document.createElement("h4");
+      bad.appendChild(document.createTextNode("Coming Soon."));
+      node.appendChild(bad);
     });
 
-    return node;
+  return node;
 }
 
 function showAbout() {
-    document.getElementById("flux_content").style.display = "none";
-    document.getElementById("about_content").style.display = "block";
+  document.getElementById("flux_content").style.display = "none";
+  document.getElementById("about_content").style.display = "block";
 }
 
 function showFlux() {
-    document.getElementById("flux_content").style.display = "block";
-    document.getElementById("about_content").style.display = "none";
+  document.getElementById("flux_content").style.display = "block";
+  document.getElementById("about_content").style.display = "none";
 }
